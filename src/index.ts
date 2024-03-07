e8c361f60b8e6c4df062975c7d38913aaf479649
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-03-07 16:35:23 +0200

    change "main" to use src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-const cors = require('cors'); // middleware
 const app = express();
 
-app.use(cors())
-
 dotenv.config()
-
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
-
 
 app.listen(process.env.PORT, () => {
   console.log('Server is listening on port 3000');
